bb99e741380370bc0337e8888bc41466c89349c2
Vivek Jethva <vivekhjtv18@gmail.com>
2023-10-11 10:41:38 -0400

    text color

--- 03ReactBgColor/src/App.jsx
+++ 03ReactBgColor/src/App.jsx
@@ -3,9 +3,53 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [color, setColor] = useState('');
+  const headingSize = '70px';
+  return (
+    <div className="w-full h-screen duration-200" style={{ color: color }}>
+      <div style={{ fontSize: headingSize }}>Hello, Everyone</div>
 
-  return <></>;
+      <div className="fixed flex flex-wrap justify-center top-19 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor('red')}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: 'red' }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor('green')}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: 'green' }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor('blue')}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: 'blue' }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => setColor('pink')}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: 'pink' }}
+          >
+            Pink
+          </button>
+          <button
+            onClick={() => setColor('black')}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: 'black' }}
+          >
+            Black
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
